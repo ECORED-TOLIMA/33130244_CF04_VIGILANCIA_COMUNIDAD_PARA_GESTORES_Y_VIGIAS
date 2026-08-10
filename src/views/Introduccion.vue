48e8937a6567e170e019077e4c590b00031cf620
Francisco Vásquez <francisjvasquez23@gmail.com>
2026-08-10 14:46:39 -0500

    Avance
    Avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -31,6 +31,7 @@
             .video
               iframe(width="560" height="315" src="https://www.youtube.com/embed/tVbNgS33Rsw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
+
 </template>
 
 <script>
